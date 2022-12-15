eb6262913ed82e86fe6c017f9c0dabf398e3874c
Aleksa Jaglicic <116374965+aleksajaglicic@users.noreply.github.com>
2022-12-15 21:05:25 +0100

    Adding main project folder

---
+++ main/start_app.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace main
+{
+    class start_app
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
