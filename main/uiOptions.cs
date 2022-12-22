cc74a4f8683ec25e16b133d6bbc3bfa338c9020e
Aleksa Jaglicic <116374965+aleksajaglicic@users.noreply.github.com>
2022-12-22 23:46:08 +0100

    Addition of new functions for UI

--- main/uiOptions.cs
+++ main/uiOptions.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using main.consumer;
 
 namespace main
 {
     class UiOptions
     {
         private int uiNum;
+        private solwin.Solwin sw = new solwin.Solwin();
+        LogicFunctions pomocna = new LogicFunctions();
+        LogicFunctions lista= new LogicFunctions();
+        private int brojac = 0;
 
         public int UINum
         {
             get { return uiNum; }
             set { uiNum = value; }
         }
-
+        
         public UiOptions(int ui)
         {
             UINum = ui;
@@ -84,6 +89,56 @@ namespace main
             }
         }
 
+        //public void ConsumerUI()
+        //{
+        //    Console.BackgroundColor = ConsoleColor.White;
+        //    Console.ForegroundColor = ConsoleColor.Black;
+
+        //    // Main Consumer UI
+        //    Console.WriteLine("Consumer UI");
+
+        //    Console.BackgroundColor = ConsoleColor.Black;
+        //    Console.ForegroundColor = ConsoleColor.White;
+
+        //    Console.WriteLine("\nPlease select one of the following:");
+        //    Console.WriteLine("\\\\\\\\ 1.Add/Erase Connection Sockets");
+        //    Console.WriteLine("\\\\\\\\ 2.View all Connected Sockets");
+        //    Console.WriteLine("\\\\\\\\ 3.Return to main menu\n");
+
+        //    Console.WriteLine("Go to option: ");
+        //    UINum = Int32.Parse(Console.ReadLine());
+            
+        //    //Selected options
+        //    if(UINum == 1)
+        //    {
+        //        Console.Clear();
+        //        AddEraseUI();
+        //    }
+        //    else if(UINum == 2)
+        //    {
+        //        Console.Clear();
+        //        ViewSockets();
+        //    }
+        //    else if(UINum == 3)
+        //    {
+        //        Console.Clear();
+        //        MainUI();
+        //    }
+        //    else
+        //    {
+        //        Console.Clear();
+        //        Console.BackgroundColor = ConsoleColor.Red;
+        //        Console.ForegroundColor = ConsoleColor.White;
+
+        //        Console.WriteLine("ERROR: ui number out of range... Try again");
+        //        ConsumerUI();
+
+        //        Console.BackgroundColor = ConsoleColor.Black;
+        //        Console.ForegroundColor = ConsoleColor.White;
+                
+        //    }
+        //}
+
         public void ConsumerUI()
         {
             Console.BackgroundColor = ConsoleColor.White;
@@ -94,27 +149,40 @@ namespace main
 
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
+            ////////
 
             Console.WriteLine("\nPlease select one of the following:");
-            Console.WriteLine("\\\\\\\\ 1.Add/Erase Connection Sockets");
-            Console.WriteLine("\\\\\\\\ 2.View all Connected Sockets");
-            Console.WriteLine("\\\\\\\\ 3.Return to main menu\n");
+            Console.WriteLine("\\\\\\\\ 0.Select Existing Sockets");
+            Console.WriteLine("\\\\\\\\ 1.Add Connection Sockets");
+            Console.WriteLine("\\\\\\\\ 2.Erase Connection Sockets");
+            Console.WriteLine("\\\\\\\\ 3.View all Connected Sockets");
+            Console.WriteLine("\\\\\\\\ 4.Return to main menu\n");
 
             Console.WriteLine("Go to option: ");
             UINum = Int32.Parse(Console.ReadLine());
-            
+
             //Selected options
-            if(UINum == 1)
+            if (UINum == 0)
             {
                 Console.Clear();
-                AddEraseUI();
+                SelectUI();
             }
-            else if(UINum == 2)
+            else if (UINum == 1)
+            {
+                Console.Clear();
+                AddUI();
+            }
+            else if (UINum == 2)
+            {
+                Console.Clear();
+                EraseUI();
+            }
+            else if (UINum == 3)
             {
                 Console.Clear();
                 ViewSockets();
             }
-            else if(UINum == 3)
+            else if (UINum == 4)
             {
                 Console.Clear();
                 MainUI();
@@ -130,18 +198,85 @@ namespace main
 
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.ForegroundColor = ConsoleColor.White;
-                
+
             }
         }
 
-        public void AddEraseUI()
+        public void SelectUI()
         {
-            //To be added
+            if (brojac < 1)
+            {
+                pomocna.Ucitaj_iz_fajla(@"Ucitavanje.txt");
+            }
+            Console.WriteLine(pomocna);
+            Console.WriteLine();
+
+            if (pomocna.arhiva.Count == 0)
+            {
+                ConsumerUI();
+            }
+            Console.WriteLine("Izaberi postojeci prikljucak na osnovu ID");
+
+            int id = Int32.Parse(Console.ReadLine());
+
+            Consumer c1 = pomocna.Find(id);
+            pomocna.Remove(id);
+            lista.Add(c1);
+            lista.Upisi_u_fajl("TestPogled.txt");
+
+            brojac++;
+
+            Console.Clear();
+            ConsumerUI();
+
         }
+
+        public void AddUI()
+        {
+            Console.WriteLine("Unesite ID od novog prikljucka ");
+
+            int id = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite naziv od novog prikljucka ");
+
+            string naziv = Console.ReadLine();
+
+            Console.WriteLine("Unesite potrosnju u kWH od novog prikljucka ");
+
+            int kwh = Int32.Parse(Console.ReadLine());
+
+            Consumer c = new Consumer(id, naziv, kwh);
+
+            lista.Add(c);
+            lista.Upisi_u_fajl("TestPogled.txt");
+            Console.WriteLine(lista);
+
+            Console.Clear();
+            ConsumerUI();
+
+        }
+
+        public void EraseUI()
+        {
+            Console.WriteLine("Unesite ID od prikljucka kojeg zelite da obriste");
+            int id = Int32.Parse(Console.ReadLine());
+
+            lista.Remove(id);
+            lista.Upisi_u_fajl("TestPogled.txt");
+            Console.WriteLine(lista);
+
+            Console.Clear();
+            ConsumerUI();
+
+        }
+
 
         public void ViewSockets()
         {
-            //To be added
+            Console.WriteLine(lista);
+            lista.Upisi_u_fajl("TestPogled.txt");
+            ConsumerUI();
+
         }
 
         public void DistributionCenterUI()
@@ -209,7 +344,7 @@ namespace main
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
 
-            // Main Consumer UI
+            // Main Solar and Wind UI
             Console.WriteLine("Solar panel and Wind generator UI");
 
             Console.BackgroundColor = ConsoleColor.Black;
@@ -262,12 +397,72 @@ namespace main
 
         public void AddEraseSolWinUI()
         {
+
             //To be added
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            // Main Solar and Wind UI
+            Console.WriteLine("Solar panel and Wind generator UI");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\nPlease select one of the following:");
+            Console.WriteLine("\\\\\\\\ 1.Add Instances of panels or generators");
+            Console.WriteLine("\\\\\\\\ 2.Erase Instances of panels or generators");
+            Console.WriteLine("\\\\\\\\ 3.Return to previous menu\n");
+
+            Console.WriteLine("Go to option: ");
+            UINum = Int32.Parse(Console.ReadLine());
+
+            //Selected options
+            if (UINum == 1)
+            {
+                Console.Clear();
+                sw.addInstance();
+                AddEraseSolWinUI();
+            }
+            else if (UINum == 2)
+            {
+                Console.Clear();
+                sw.eraseInstance();
+                AddEraseSolWinUI();
+            }
+            else if (UINum == 3)
+            {
+                Console.Clear();
+                SolarWindUI();
+            }
+            else
+            {
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("ERROR: ui number out of range... Try again");
+                SolarWindUI();
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+
         }
 
         public void ViewPanelGen()
         {
             //To be added
+            sw.viewList();
+            Console.WriteLine("Return to previous menu? (1 for yes)");
+            UINum = Int32.Parse(Console.ReadLine());
+
+            if(uiNum == 1)
+            {
+                Console.Clear();
+                SolarWindUI();
+            }
+
         }
 
         public void ChangeSunWind()
@@ -280,7 +475,7 @@ namespace main
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
 
-            // Main Distribution Center UI
+            // Main Power Plant UI
             Console.WriteLine("Power Plant UI");
 
             Console.BackgroundColor = ConsoleColor.Black;
