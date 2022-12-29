f9018cf384a3691cdc0244b3153196628abe8590
Aleksa Jaglicic <116374965+aleksajaglicic@users.noreply.github.com>
2022-12-29 22:41:15 +0100

    Addition of new functions, ConsoleTables pkg

--- main/solwin/solwin.cs
+++ main/solwin/solwin.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using ConsoleTables;
+using ConsoleTables;
 
 namespace main.solwin
 {
@@ -13,15 +13,155 @@ namespace main.solwin
         List<Instance> instances;
         public System.Random random = new System.Random();
         public DateTime timestamp = new DateTime();
-        //string file = Path.Combine(Environment.CurrentDirectory, @"main\solwin\", "SolarWindInstances.txt");
-        public string file = @"C:\Users\aleksa2\source\main\solwin\SolarWindInstances.txt";
+        public string file = Environment.CurrentDirectory + "/SolarWindInstances.txt";
 
         public Solwin()
         {
             instances = new List<Instance>();
         }
 
-        public void addInstance(int c = 0, int cP = 0, int P = 0)
+        public void readFromFile(List<Instance> instances)
+        {
+            StreamReader sr = null;
+            string lines;
+            int idR;
+            int idSW;
+            int powerR;
+            DateTime timeChanged;
+
+            instances.Clear();
+
+            try
+            {
+                sr = new StreamReader(file);
+
+                while ((lines = sr.ReadLine()) != null)
+                {
+                    string[] lineParts = lines.Split('|');
+                    idR = Int32.Parse(lineParts[0]);
+                    powerR = Int32.Parse(lineParts[1]);
+                    idSW = Int32.Parse(lineParts[2]);
+                    timeChanged = Convert.ToDateTime(lineParts[3]);
+
+                    Instance instanceR = new Instance(idR, idSW, powerR, timeChanged);
+                    instances.Add(instanceR);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+            }
+        }
+
+        public void readFromFileTest(List<Instance> instances, string file)
+        {
+            StreamReader sr = null;
+            string lines;
+            int idR;
+            int idSW;
+            int powerR;
+            DateTime timeChanged;
+
+            instances.Clear();
+
+            try
+            {
+                sr = new StreamReader(file);
+
+                while ((lines = sr.ReadLine()) != null)
+                {
+                    string[] lineParts = lines.Split('|');
+                    idR = Int32.Parse(lineParts[0]);
+                    powerR = Int32.Parse(lineParts[1]);
+                    idSW = Int32.Parse(lineParts[2]);
+                    timeChanged = Convert.ToDateTime(lineParts[3]);
+
+                    Instance instanceR = new Instance(idR, idSW, powerR, timeChanged);
+                    instances.Add(instanceR);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+            }
+        }
+
+        public void writeToFile(int id, int choice, int power)
+        {
+            Instance instance = new Instance(id, choice, power, DateTime.Now);
+
+            StreamWriter sw = null;
+            string text = "";
+            text += instance.ToString();
+            text += "\n";
+
+            try
+            {
+                sw = new StreamWriter(file, true);
+                sw.Write(text);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                try
+                {
+                    if (sw != null)
+                    {
+                        sw.Close();
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+
+        public void writeToFileTest(int id, int choice, int power, DateTime dateTime, string file)
+        {
+            Instance instance = new Instance(id, choice, power, dateTime);
+            StreamWriter sw = null;
+            string text = "";
+            text += instance.ToString();
+            text += "\n";
+
+            try
+            {
+                sw = new StreamWriter(file, true);
+                sw.Write(text);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                try
+                {
+                    if (sw != null)
+                    {
+                        sw.Close();
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+
+        public void addInstance(int c = 0, int cP = 0)
         {
             int choice;
             int choicePower;
@@ -57,8 +197,6 @@ namespace main.solwin
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.ForegroundColor = ConsoleColor.White;
                 power = Int32.Parse(Console.ReadLine());
-                //Unit Testing
-                //power = P;
                 Console.Clear();
             }
             else
@@ -66,73 +204,19 @@ namespace main.solwin
                 power = random.Next(0, 100);
             }
 
-            StreamReader sr = null;
-            string lines;
-            int idR;
-            int idSW;
-            int powerR;
-            DateTime timeChanged;
+            readFromFile(instances);
+            int id = instances.Count;
+            writeToFile(id, choice, power);
+        }
 
-            instances.Clear();
-
-            try
-            {
-                sr = new StreamReader(file);
-
-                while ((lines = sr.ReadLine()) != null)
-                {
-                    string[] lineParts = lines.Split('|');
-                    idR = Int32.Parse(lineParts[0]);
-                    powerR = Int32.Parse(lineParts[1]);
-                    idSW = Int32.Parse(lineParts[2]);
-                    timeChanged = Convert.ToDateTime(lineParts[3]);
-                    //Unit Testing
-                    //timeChanged = System.Convert.ToDateTime("22/12/2022 7:31:20 pm");
-
-                    Instance instanceR = new Instance(idR, idSW, powerR, timeChanged);
-                    instances.Add(instanceR);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                if (sr != null)
-                {
-                    sr.Close();
-                }
-            }
+        public void addInstanceTest(int c = 0, int cP = 0, string file = "")
+        {
+            int choice = c;
+            int choicePower = cP;
+            DateTime dateTime = Convert.ToDateTime("22/12/2022 7:31:20 pm");
 
             int id = instances.Count;
-
-            Instance instance = new Instance(id, choice, power, DateTime.Now);
-
-            StreamWriter sw = null;
-            string text = "";
-
-            text += instance.ToString();
-            text += "\n";
-
-            try
-            {
-                sw = new StreamWriter(file, true);
-                sw.Write(text);
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.StackTrace);
-            }
-            finally
-            {
-                try
-                { 
-                    sw.Close(); 
-                } 
-                catch (Exception) { }
-            }
+            writeToFileTest(id, choice, choicePower, dateTime, file);
         }
 
         public void eraseInstance()
@@ -150,44 +234,11 @@ namespace main.solwin
             eraseId = Int32.Parse(Console.ReadLine());
             Console.Clear();
 
-            StreamReader sr = null;
-            int id;
-            int idSW;
-            int power;
-            string lines;
-            DateTime timeChanged;
+            readFromFile(instances);
 
-            try
+            for (int i = 0; i < instances.Count; i++)
             {
-                sr = new StreamReader(file);
-
-                while ((lines = sr.ReadLine()) != null)
-                {
-                    string[] lineParts = lines.Split('|');
-                    id = Int32.Parse(lineParts[0]);
-                    power = Int32.Parse(lineParts[1]);
-                    idSW = Int32.Parse(lineParts[2]);
-                    timeChanged = Convert.ToDateTime(lineParts[3]);
-
-                    Instance instance = new Instance(id, idSW, power, timeChanged);
-                    instances.Add(instance);
-                }
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                if(sr != null)
-                {
-                    sr.Close();
-                }
-            }
-
-            for(int i = 0; i < instances.Count; i++)
-            {
-                if(eraseId == instances[i].ID)
+                if (eraseId == instances[i].ID)
                 {
                     instances.RemoveAt(i);
 
@@ -199,7 +250,6 @@ namespace main.solwin
                         text += instances[j].ToString();
                         text += "\n";
                     }
-
 
                     File.Create(file).Close();
 
@@ -218,7 +268,51 @@ namespace main.solwin
                         {
                             sw.Close();
                         }
-                        catch (Exception) {}
+                        catch (Exception) { }
+                    }
+                }
+            }
+        }
+
+        public void eraseInstanceTest(int eraseId, string file)
+        {
+            List<Instance> instances = new List<Instance>();
+
+            readFromFileTest(instances, file);
+
+            for (int i = 0; i < instances.Count; i++)
+            {
+                if (eraseId == instances[i].ID)
+                {
+                    instances.RemoveAt(i);
+
+                    StreamWriter sw = null;
+                    string text = "";
+
+                    for (int j = 0; j < instances.Count; j++)
+                    {
+                        text += instances[j].ToString();
+                        text += "\n";
+                    }
+
+                    File.Create(file).Close();
+
+                    try
+                    {
+                        sw = new StreamWriter(file);
+                        sw.Write(text);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.StackTrace);
+                    }
+                    finally
+                    {
+                        try
+                        {
+                            sw.Close();
+                        }
+                        catch (Exception) { }
                     }
                 }
             }
@@ -227,53 +321,112 @@ namespace main.solwin
         public void viewList()
         {
             instances.Clear();
-            StreamReader sr = null;
-            string lines;
-            int id;
-            int power;
-            int idSW;
-            DateTime timeChanged;
-            //var table = new ConsoleTable("Id:", "Power: ", "Type", "TimeStamp");
+            var table = new ConsoleTable("Id:", "Power: ", "Type", "TimeStamp");
+            readFromFile(instances);
+
+            ConsoleTable.From<Instance>(instances).Write();
+        }
+
+        public bool existsById(int id, List<Instance> instances)
+        {
+            readFromFile(instances);
+            
+            for(int i = 0; i < instances.Count; i++)
+            {
+                if(id == instances[i].ID)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void insertChange(List<Instance> instances)
+        {
+            string text = "";
+            string file = Environment.CurrentDirectory + "/SolarWindInstances.txt";
+            StreamWriter sw = null;
+
+            for (int i = 0; i < instances.Count; i++)
+            {
+                text += instances[i].ToString();
+                if( i != instances.Count - 1)
+                {
+                    text += "\n";
+                }
+            }
 
             try
             {
-                sr = new StreamReader(file);
-
-                while ((lines = sr.ReadLine()) != null)
-                {
-                    string[] lineParts = lines.Split('|');
-                    id = Int32.Parse(lineParts[0]);
-                    power = Int32.Parse(lineParts[1]);
-                    idSW = Int32.Parse(lineParts[2]);
-                    timeChanged = Convert.ToDateTime(lineParts[3]);
-
-                    Instance instance = new Instance(id, idSW, power, timeChanged);
-                    instances.Add(instance);
-                }
+                File.WriteAllText(file, string.Empty);
+                sw = new StreamWriter(file);
+                sw.WriteLine(text);
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
             }
             finally
             {
-                if (sr != null)
+                try
                 {
-                    sr.Close();
+                    if (sw != null)
+                    {
+                        sw.Close();
+                    }
                 }
+                catch (Exception) { }
             }
+        }
 
-           //ConsoleTable.From<Instance>(instances).Write();
-            Console.WriteLine("Id:\t\tPower:\t\tIdSW:\t\tTimeStamp:");
-            foreach(Instance instance in instances)
+        public void change(int id, int power, List<Instance> instances)
+        {
+            for (int i = 0; i < instances.Count; i++)
             {
-                Console.WriteLine(instance);
+                if (id == instances[i].ID)
+                {
+                    instances[i].Power = power;
+                    instances[i].TimeChanged = DateTime.Now;
+                }
+
+                insertChange(instances);
             }
         }
 
         public void changePower()
         {
+            viewList();
+            Console.WriteLine();
 
+            int id;
+            int power;
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Please enter id of instance you wish to change the power of:");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            id = Int32.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            readFromFile(instances);
+            if(existsById(id, instances))
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("Please enter the power you wish to change to:");
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                power = Int32.Parse(Console.ReadLine());
+
+                change(id, power, instances);
+            }
         }
 
         public override bool Equals(object obj)
