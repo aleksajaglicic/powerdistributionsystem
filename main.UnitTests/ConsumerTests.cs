d07bc1f86e4190f6079044fa679a297ed11846f4
aleksajaglicic <aleksajaglicic070@gmail.com>
2023-01-13 21:07:50 +0100

    Rework Part 2

--- main.UnitTests/ConsumerTests.cs
+++ main.UnitTests/ConsumerTests.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using powerdistributionsystem.consumer;
+using Moq;
 
 namespace powerdistributionsystem.UnitTests
 {
@@ -87,6 +88,44 @@ namespace powerdistributionsystem.UnitTests
 
             //Assert
             Assert.AreEqual(consumer1.ToString()+"\n\r\n", File.ReadAllText(file));
+        }
+
+        Mock<LogicFunctions> mockLogicFunctionService;
+
+        LogicFunctions systemUnderTest;
+
+        [TestCase(14, "Computer", 40)]
+
+        public void addInstance_ToList(int id, string naziv, int kwh)
+        {
+            mockLogicFunctionService = new Mock<LogicFunctions>();
+            Consumer c1 = new Consumer(id, naziv, kwh);
+            mockLogicFunctionService = new Mock<LogicFunctions>(MockBehavior.Strict);
+
+
+            systemUnderTest = new LogicFunctions();
+            var result = systemUnderTest.Add(c1);
+
+            Assert.AreEqual(true, result);
+
+            mockLogicFunctionService.VerifyAll();
+        }
+
+        [TestCase(14, "Computer", 40)]
+
+        public void removeInstance_FromList(int id, string naziv, int kwh)
+        {
+            mockLogicFunctionService = new Mock<LogicFunctions>();
+            Consumer c1 = new Consumer(id, naziv, kwh);
+            mockLogicFunctionService = new Mock<LogicFunctions>(MockBehavior.Strict);
+
+
+            systemUnderTest = new LogicFunctions();
+            var result = systemUnderTest.Remove(c1.Id);
+
+            Assert.AreEqual(false, result);
+
+            mockLogicFunctionService.VerifyAll();
         }
     }
 }
